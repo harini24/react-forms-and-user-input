0f70044c2272f90917d70d4b3e448f34a62221d9
harini <harinisiva1997@gmail.com>
2021-05-27 22:37:53 +0530

    handling is toutched state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,27 +3,31 @@ import { useRef, useState } from 'react'
 const SimpleInput = (props) => {
   const [enteredname, setEnteredName] = useState('')
   const nameInputRef = useRef()
-  const [enteredNameIsValid, setEmteredNameIsValid] = useState(true)
+  // const [enteredNameIsValid, setEmteredNameIsValid] = useState(true) // its not menaningful to set a invalid value as true
+  const [enteredNameIsValid, setEmteredNameIsValid] = useState(false)
+  const [enterednameTouched, setEnteredNameTouched] = useState(false)
+
   const formSubmisstionhandler = (e) => {
     e.preventDefault();
-
+    setEnteredNameTouched(true)
     if (enteredname.trim() === '') {
       setEmteredNameIsValid(false)
       return
     }
-    setEmteredNameIsValid(false)
+    setEmteredNameIsValid(true)
     console.log(enteredname)
     const enteredNameVal = nameInputRef.current.value
     console.log(enteredNameVal)
   }
 
-  const nameInputClasses = enteredNameIsValid ? 'form-control' : ' form-control invalid'
+  const nameInputIsInvalue = !enteredNameIsValid && enterednameTouched
+  const nameInputClasses = nameInputIsInvalue ? 'form-control invalid' : ' form-control '
   return (
     <form onSubmit={formSubmisstionhandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' value={enteredname} onChange={(e) => setEnteredName(e.target.value)} />
-        {!enteredNameIsValid && <p className="error-text">Name should not be empty</p>}
+        <input ref={nameInputRef} type='text' id='name' value={enteredname} onChange={(e) => { setEnteredName(e.target.value) }} />
+        {nameInputIsInvalue && <p className="error-text">Name should not be empty</p>}
       </div>
       <div className="form-actions">
         <button type="submit">Submit</button>
